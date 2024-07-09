2c71393317e212ba033ce8d151a4f464187ff1b0
Aminul Hoque NuSu <158472597+aminulnusu@users.noreply.github.com>
2024-07-10 01:28:02 +0600

    Angular new project practice

---
+++ Angular/second/src/app/location/location.service.ts
@@ -0,0 +1,34 @@
+import { Location } from './location.model';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LocationService {
+
+  baseUrl: string = "http://localhost:3000/locations";
+
+
+  constructor(private httpCline: HttpClient) { }
+
+  getAllLocation(): Observable<any> {
+
+    return this.httpCline.get(this.baseUrl);
+  }
+
+  createLocation(location: Location): Observable<any> {
+    return this.httpCline.post(this.baseUrl, location);
+
+  }
+
+
+  deleteLocation(id: string): Observable<any> {
+    return this.httpCline.delete(this.baseUrl + "/" + id);
+    //  http://localhost:3000/locations/id
+  }
+
+
+
+}
